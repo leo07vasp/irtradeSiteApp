2874ce52b49bb46ee421ef74b44c9379c6d368fb
Leonardo Rodrigues <leonardo@bis2bis.com.br>
2016-07-29 09:37:32 -0300

    calculos bmf e bovespa

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ valorBase1 = 1078.8;
 function calcPorcentagemOnAtualValue(porcent, value){
 	var base = parseFloat(value);
 	var basePercent = base/100;
-	console.log(basePercent);
-	console.log(porcent);
 	return basePercent*porcent;
 }
 
 function multiplyValue(nMulti, value){
-	return (nMulti*value).toFixed(0)
+	return (nMulti*value);
 }
 
 function init(){
@@ -25,35 +23,47 @@ jQuery(document).ready(function(){
 
 
 	jQuery(".qtdPeriodos").on("keyup mouseup" , function(){
-		console.log(multiplyValue(valorBase1, jQuery(this).val()));
-		jQuery("#resultado").html(multiplyValue(valorBase1, jQuery(this).val()));
+		if(typeof LastResult == 'undefined'){
+			jQuery("#resultado").html(multiplyValue(valorBase1, jQuery(this).val()));
+		}else{
+			jQuery("#resultado").html(calcPorcentagemOnAtualValue(ToPercent , multiplyValue(valorBase1, jQuery(this).val())));
+		}
+		
 	});
 	
 
 	jQuery(".qtdCorretoras").on("keyup mouseup" , function(){
 		valorAtual = parseFloat(jQuery("#resultado").text());
 		if(jQuery(this).val() >= 5){
-			var ToPercent = 100;
+			ToPercent = 100;
 		}else{
 			switch (parseInt(jQuery(this).val())) {
 			  case 1:
-			    var ToPercent = 60;
+				 ToPercent = 60;
 			    break;
 			  case 2:
-			    var ToPercent = 70;
+			     ToPercent = 70;
 			    break;
 			  case 3:
-			    var ToPercent = 80;
+			     ToPercent = 80;
 			    break;
 			  case 4:
-			    var ToPercent = 90;
+			     ToPercent = 90;
 			    break;
 			  default:
-			    var ToPercent = 100;
+			     ToPercent = 100;
 			}
 		}
 		var valueBaseTotoal = multiplyValue(valorBase1, jQuery(".qtdPeriodos").val());
 		jQuery("#resultado").html(calcPorcentagemOnAtualValue(ToPercent , valueBaseTotoal));
+		LastResult = calcPorcentagemOnAtualValue(ToPercent , valueBaseTotoal);
 	});	
+
+
+	jQuery(".mercadotype").on("change", function(){
+		if(typeof LastResult != 'undefined'){
+			jQuery("#resultado").html(calcPorcentagemOnAtualValue(jQuery(this).val(), LastResult));
+		}
+	});
 
 });
