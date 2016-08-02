eb79421d5c97b174a95691f2951a00e68be7779e
Leonardo Silva <leonardosilva@MBP-de-Leonardo.home>
2016-08-02 19:05:33 -0300

    ajustando contas retroativas

--- app.js
+++ app.js
@@ -14,30 +14,12 @@ function multiplyValue(nMulti, value){
 	return (nMulti*value);
 }
 
-function init(){
-
-}
-
-
-jQuery(document).ready(function(){
-
-
-	jQuery(".qtdPeriodos").on("keyup mouseup" , function(){
-		if(typeof LastResult == 'undefined'){
-			jQuery("#resultado").html(multiplyValue(valorBase1, jQuery(this).val()));
-		}else{
-			jQuery("#resultado").html(calcPorcentagemOnAtualValue(ToPercent , multiplyValue(valorBase1, jQuery(this).val())));
-		}
-		
-	});
+function getValueTopercent(){
 	
-
-	jQuery(".qtdCorretoras").on("keyup mouseup" , function(){
-		valorAtual = parseFloat(jQuery("#resultado").text());
-		if(jQuery(this).val() >= 5){
+		if(jQuery(".qtdCorretoras").val() >= 5){
 			ToPercent = 100;
 		}else{
-			switch (parseInt(jQuery(this).val())) {
+			switch (parseInt(jQuery(".qtdCorretoras").val())) {
 			  case 1:
 				 ToPercent = 60;
 			    break;
@@ -54,16 +36,37 @@ jQuery(document).ready(function(){
 			     ToPercent = 100;
 			}
 		}
+
+		return ToPercent;
+}
+
+
+jQuery(document).ready(function(){
+
+
+	jQuery(".qtdPeriodos").on("keyup mouseup" , function(){
+		if(typeof LastResult == 'undefined'){
+			jQuery("#resultado").html(multiplyValue(valorBase1, jQuery(this).val()));
+		}else{
+			jQuery("#resultado").html(calcPorcentagemOnAtualValue(getValueTopercent() , multiplyValue(valorBase1, jQuery(this).val())) * jQuery(".mercadotype").val());
+		}
+		
+	});
+	
+
+	jQuery(".qtdCorretoras").on("keyup mouseup" , function(){
+		
+
 		var valueBaseTotoal = multiplyValue(valorBase1, jQuery(".qtdPeriodos").val());
-		jQuery("#resultado").html(calcPorcentagemOnAtualValue(ToPercent , valueBaseTotoal));
-		LastResult = calcPorcentagemOnAtualValue(ToPercent , valueBaseTotoal);
+		jQuery("#resultado").html(calcPorcentagemOnAtualValue(getValueTopercent() , valueBaseTotoal) * jQuery(".mercadotype").val());
+		LastResult = calcPorcentagemOnAtualValue(getValueTopercent() , valueBaseTotoal);
 	});	
 
 
 	jQuery(".mercadotype").on("change", function(){
-		if(typeof LastResult != 'undefined'){
-			jQuery("#resultado").html(calcPorcentagemOnAtualValue(jQuery(this).val(), LastResult));
-		}
+		var valueBaseTotoal = multiplyValue(valorBase1, jQuery(".qtdPeriodos").val());
+		jQuery("#resultado").html(calcPorcentagemOnAtualValue(getValueTopercent() , valueBaseTotoal) * jQuery(".mercadotype").val());
+		
 	});
 
 });
